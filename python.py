1dc2e9003b3e51642c4a3e4db5c0ba82708b9023
renu216527 <renukaswamysk63@gmail.com>
2024-09-08 00:17:50 +0530

    commit is message to show

---
+++ python.py
@@ -0,0 +1,12 @@
+import time
+timestamp=int(time.strftime('%H'))
+print(timestamp)
+if(timestamp<12):
+    print("Goodmorning sir")
+elif (timestamp>=12 and timestamp<=13):
+    print("Good afternoon sir")
+elif (timestamp>13 and timestamp<=19):
+    print("Good evening sir")
+else:
+    print("Good night sir")
+
---
+++ python1.py
@@ -0,0 +1,20 @@
+'''for i in range(19):
+    print(i)
+nums={"renu","chiru","swamy"}
+for num in nums:
+    print(num)
+    for i in num:
+        print(i)
+
+for i in range(1,12,2):
+    print(i)
+i=0
+while(i<=3):
+    print(i)
+    i=i+1
+print("End")'''
+i=int(input("Enter the number"))
+while(i<=9):
+    i=int(input("Enter the number:"))
+    print(i)
+print("end")
